9f1fee38c5300107756886325bc4e86731147f78
Max Inden <mail@max-inden.de>
2020-07-05 10:07:25 +0200

    src/lib: Set yamux receive window to max OS TCP receive window

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,48 @@ pub fn build_transport(
     let mut yamux_config = yamux::Config::default();
     yamux_config.set_window_update_mode(yamux::WindowUpdateMode::OnRead);
 
+    // The default TCP receive window (minimum, default, maximum) on my OS
+    // (Debian) is:
+    //
+    // $ cat /proc/sys/net/ipv4/tcp_rmem
+    // 4096    131072  6291456
+    //
+    // Possible Bandwidth of a connection ignoring all overheads of TCP would be
+    // calculated with:
+    //
+    // Bandwidth (mBit/s) = (Receive window in bit) / (latency in s) / 1_000_000
+    //
+    // Ping latency via `localhost` is around 0.09 ms:
+    //
+    // $ ping localhost
+    // 64 bytes from localhost (::1): icmp_seq=2 ttl=64 time=0.095 ms
+    // 64 bytes from localhost (::1): icmp_seq=3 ttl=64 time=0.087 ms
+    //
+    // Thus the bandwidth with the maximum receive window would be:
+    //
+    // ((6291456*8) / (0,09/1000)) / 1000000 = 559_240 mBit/s
+    //
+    // An iperf run on localhost achieves around 60 gBit/sec:
+    //
+    // $ iperf -c 127.0.0.1
+    // [  3]  0.0-10.0 sec  68.4 GBytes  58.8 Gbits/sec
+    //
+    // A libp2p-perf run with the default yamux receive window settings (256
+    // kByte) achieves a bandwidth of 30 mBit/s:
+    //
+    // $ cargo run --bin client --release -- --server-address /ip4/127.0.0.1/tcp/9992
+    // Interval        Transfer        Bandwidth
+    // 0 s - 10.08 s   35 MBytes       27.78 MBit/s
+    //
+    // With the yamux receive window set to the OS max receive window (6291456
+    // bytes) libp2p-perf runs as fast as 500 mBit/s:
+    //
+    // $ cargo run --bin client --release -- --server-address /ip4/127.0.0.1/tcp/9992
+    // Interval        Transfer        Bandwidth
+    // 0 s - 10.00 s   614 MBytes      491.19 MBit/s
+    yamux_config.set_receive_window(6_291_456);
+    yamux_config.set_max_buffer_size(6_291_456);
+
     Ok(dns::DnsConfig::new(tcp::TcpConfig::new())?
         .upgrade(core::upgrade::Version::V1)
         .authenticate(
