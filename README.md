659c5f38a92d506aa62db7d3bf3307d2e043a9c7
Max Inden <mail@max-inden.de>
2020-09-22 18:36:28 +0200

    run.sh: Update to test both noise and plaintext

--- README.md
+++ README.md
@@ -22,20 +22,44 @@ $ ./run.sh
 # Start Rust and Golang servers.
 
 # Rust -> Rust
+
+## Transport protocol noise
 Interval        Transfer        Bandwidth
-0 s - 10.00 s   13338 MBytes    10670.28 MBit/s
+0 s - 10.06 s   1862 MBytes     1480.67 MBit/s
+
+## Transport protocol plaintext
+Interval        Transfer        Bandwidth
+0 s - 10.00 s   10267 MBytes    8209.56 MBit/s
 
 # Rust -> Golang
+
+## Transport protocol noise
 Interval        Transfer        Bandwidth
-0 s - 10.00 s   20365 MBytes    16292.00 MBit/s
+0 s - 10.00 s   7740 MBytes     6191.96 MBit/s
+
+## Transport protocol plaintext
+Interval        Transfer        Bandwidth
+0 s - 10.00 s   17588 MBytes    14070.39 MBit/s
 
 # Golang -> Rust
+
+## Transport protocol noise
 Interval        Transfer        Bandwidth
-0s - 10.00 s    16223 MBytes    12975.88 MBit/s
+0s - 10.03 s    2349 MBytes     1873.96 MBit/s
+
+## Transport protocol plaintext
+Interval        Transfer        Bandwidth
+0s - 10.00 s    17213 MBytes    13768.89 MBit/s
 
 # Golang -> Golang
+
+## Transport protocol noise
 Interval        Transfer        Bandwidth
-0s - 10.00 s    23166 MBytes    18532.68 MBit/s
+0s - 10.00 s    5983 MBytes     4786.30 MBit/s
+
+## Transport protocol plaintext
+Interval        Transfer        Bandwidth
+0s - 10.00 s    21399 MBytes    17115.38 MBit/s
 
 
 $ cat /proc/cpuinfo | grep "model name" | head -1
--- run.sh
+++ run.sh
@@ -9,27 +9,48 @@ set -u
 # Make sure to kill all background tasks when exiting.
 trap "kill 0" EXIT
 
+TransportSecurityProtocols=( noise plaintext )
+
 echo "# Start Rust and Golang servers."
-./rust/target/release/server --listen-address /ip4/0.0.0.0/tcp/9992 --private-key-pkcs8 rust/test.pk8 > /dev/null 2>&1 &
-./golang/go-libp2p-perf --fake-crypto-seed --listen-address /ip4/0.0.0.0/tcp/9993 > /dev/null 2>&1 &
+./rust/target/release/server --private-key-pkcs8 rust/test.pk8 --listen-address /ip4/0.0.0.0/tcp/9992 > /dev/null 2>&1 &
+./golang/go-libp2p-perf --fake-crypto-seed --listen-address /ip4/0.0.0.0/tcp/9993 --transport-security noise > /dev/null 2>&1 &
+./golang/go-libp2p-perf --fake-crypto-seed --listen-address /ip4/0.0.0.0/tcp/9994 --transport-security plaintext > /dev/null 2>&1 &
 
 sleep 1
 
-echo ""
+echo
 echo "# Rust -> Rust"
-./rust/target/release/client --server-address /ip4/127.0.0.1/tcp/9992
+for Protocol in ${TransportSecurityProtocols[*]}
+do
+    echo
+    echo "## Transport protocol $Protocol"
+    ./rust/target/release/client --server-address /ip4/127.0.0.1/tcp/9992 --transport-security $Protocol
+done
 
-echo ""
+echo
 echo "# Rust -> Golang"
-./rust/target/release/client --server-address /ip4/127.0.0.1/tcp/9993
+echo
+echo "## Transport protocol noise"
+./rust/target/release/client --server-address /ip4/127.0.0.1/tcp/9993 --transport-security noise
+echo
+echo "## Transport protocol plaintext"
+./rust/target/release/client --server-address /ip4/127.0.0.1/tcp/9994 --transport-security plaintext
 
-
-echo ""
+echo
 echo "# Golang -> Rust"
-./golang/go-libp2p-perf --server-address /ip4/127.0.0.1/tcp/9992/p2p/Qmcqq9TFaYbb94uwdER1BXyGfCFY4Bb1gKozxNyVvLvTSw
+for Protocol in ${TransportSecurityProtocols[*]}
+do
+    echo
+    echo "## Transport protocol $Protocol"
+    ./golang/go-libp2p-perf --server-address /ip4/127.0.0.1/tcp/9992/p2p/Qmcqq9TFaYbb94uwdER1BXyGfCFY4Bb1gKozxNyVvLvTSw --transport-security $Protocol
+done
 
-
-echo ""
+echo
 echo "# Golang -> Golang"
-./golang/go-libp2p-perf --server-address /ip4/127.0.0.1/tcp/9993/p2p/12D3KooWL3XJ9EMCyZvmmGXL2LMiVBtrVa2BuESsJiXkSj7333Jw
+echo
+echo "## Transport protocol noise"
+./golang/go-libp2p-perf --server-address /ip4/127.0.0.1/tcp/9993/p2p/12D3KooWL3XJ9EMCyZvmmGXL2LMiVBtrVa2BuESsJiXkSj7333Jw --transport-security noise
+echo
+echo "## Transport protocol plaintext"
+./golang/go-libp2p-perf --server-address /ip4/127.0.0.1/tcp/9994/p2p/12D3KooWL3XJ9EMCyZvmmGXL2LMiVBtrVa2BuESsJiXkSj7333Jw --transport-security plaintext
 
